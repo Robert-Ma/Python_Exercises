94c3ff9d48619d801dea38e69cda8a8e011214e5
Robert Ma <Ma-Wei@outlook.com>
2018-01-25 21:15:10 +1100

    Define a function count_words() to complete exercise 5.

--- Data_Structures/Script03.py
+++ Data_Structures/Script03.py
@@ -9,3 +9,30 @@ words = ['red', 'green', 'black', 'pink', 'black', 'white', 'black', 'eyes',
          'white', 'black', 'orange', 'pink', 'pink', 'red', 'red', 'white', 'orange',
          'white', "black", 'pink', 'green', 'green', 'pink', 'green', 'pink',
          'white', 'orange', "orange", 'red']
+
+# Method 1: define a function
+def count_words(sequences, num):
+    """
+    To count the most common words of sequences.
+    sequences: sequences need to be counted
+    num: return top num words.
+    """
+
+    counts = {}
+
+    # To count words
+    for x in sequences:
+        if x in counts:
+            counts[x] += 1
+        else:
+            counts[x] = 1
+
+    items = counts.items()    # To get all items with tuple
+    sortedItems = tuple(sorted(items, key = lambda element: (element[1],element[0]),reverse = True))    # To sort tuple by values
+
+    return sortedItems[:top]
+
+
+
+if __name__ == '__main__':
+    print(list(count_words(words,5)))
