062f2941290b95bde2d4be08e77536d6abb6930d
Robert Ma <Ma-Wei@outlook.com>
2018-01-26 13:01:45 +1100

    Add library collections descriptions.

---
+++ Data_Structures/Exercise06.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Write a Python program to find the class wise roll number from a tuple-of-tuples.
+Using the python standard library: collections:
+    1) namedtuple(): factory function for creating tuple subclasses with named fields
+    2) deque: list-like container with fast appends and pops on either end
+    3) Counter: dict subclass for counting hashable objects
+    4) OrderedDict: dict subclass that remembers the order entrier were added
+    5) defaultdict: dict subclass that calls a factory function to supply missing values
+"""
+
