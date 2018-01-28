f8809aa1d1a800fadf44d408d45b6945b395ab18
Robert Ma <Ma-Wei@outlook.com>
2018-01-28 12:37:35 +1100

    complete reverse sort of bubble sort algorithm

--- Search_and_Sorting/Exercise03.py
+++ Search_and_Sorting/Exercise03.py
@@ -3,13 +3,16 @@ Exercise 03: Write a Python program to sort a list of elements using the bubble
 sort algorithm.
 """
 
-def bubbleSort(aList):
+import operator
+
+def bubbleSort(aList, reverse=False):
+    oper = operator.lt if reverse else operator.gt
     flag = False
     while not flag:
         flag = True
         n = 0
         for i in range(len(aList) - 1 - n):
-            if aList[i] > aList[i + 1]:
+            if oper(aList[i], aList[i + 1]):
                 # swap the two elements
                 (aList[i], aList[i + 1]) = (aList[i + 1], aList[i])
                 flag = False
@@ -22,7 +25,9 @@ if __name__ =='__main__':
     print(bubbleSort(L))
 
     L2 = [1, 2, 4, 5, 8]
-    print(bubbleSort(L2 ))
+    print(bubbleSort(L2))
+    print(bubbleSort(L2, reverse=True))
 
     L3 = [5, 1, 4, 1, 2, 8, 5]
     print(bubbleSort(L3))
+    print(bubbleSort(L3, reverse=True))
